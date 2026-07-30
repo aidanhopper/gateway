0d6e251698adefa320399499b86482c91ae83a5c
Aidan Hopper <aidanhop1@gmail.com>
2026-07-29 22:07:19 -0700

    fix: add missing config package import to api

--- internal/api/api.go
+++ internal/api/api.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/aidanhopper/gateway/internal/acme"
+	"github.com/aidanhopper/gateway/internal/config"
 	"github.com/aidanhopper/gateway/internal/firewall"
 	"github.com/aidanhopper/gateway/internal/gateway"
 )
